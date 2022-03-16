162cfd3c949ad0c0a5123f8336352325e3cfe399
Iryna Loza <iryna.loza@shopify.com>
2022-03-15 18:06:40 -0700

    Update movies display

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,13 @@
 module MoviesHelper
+  def release_date(movie)
+    movie.released_on.strftime('%Y %B %d')
+  end
+
+  def total_gross(movie)
+    if movie.flop?
+      'Flop!'
+    else
+      number_to_currency(movie.total_gross, precision: 0)
+    end
+  end
 end
